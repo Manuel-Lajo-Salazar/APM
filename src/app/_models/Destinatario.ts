3a6fb7318812d5ae2d7e2bfb842b49818bb44cbb
Eisson Alipio <eisson.alipio@am.jll.com>
2018-08-29 16:54:37 -0500

    Destinatario and Remitente added.

---
+++ src/app/_models/Destinatario.ts
@@ -0,0 +1,8 @@
+export class Destinatario {
+    constructor(
+        public id: number = null,
+        public razonSocial: string = null,
+        public ruc: string = null,
+        public direccion: string = null
+    ) {}
+}
---
+++ src/app/_models/Remitente.ts
@@ -0,0 +1,8 @@
+export class Remitente {
+    constructor(
+        public id: number = null,
+        public razonSocial: string = null,
+        public ruc: string = null,
+        public direccion: string = null
+    ) {}
+}
